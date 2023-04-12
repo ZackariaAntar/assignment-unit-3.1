b127f5e6c27439247bfb6903bfb358908d98305f
Zackaria Antar <antarzack@gmail.com>
2023-04-12 15:48:38 -0500

    forgot a few semicolons *smh*

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-let foodsArray = ['Sushi','Tacos','Couscous','Soup']
+let foodsArray = ['Sushi','Tacos','Couscous','Soup'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-console.log('My favorite foods are:', foodsArray)
+console.log('My favorite foods are:', foodsArray);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -22,19 +22,19 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-console.log('Number of foods are:', foodsArray.length)
+console.log('Number of foods are:', foodsArray.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log('First animal is', animalArray[0]);
 
 // 3.a. TODO: Log the second animal in the array 
-console.log('The second animal is', animalArray[1])
+console.log('The second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-console.log('The last animal on the list is', animalArray[3])
+console.log('The last animal on the list is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
